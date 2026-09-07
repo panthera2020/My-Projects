1b9a2d1f51ed30934b1b615b9f07e421a3b5e200
panthera2020 <diranoye2020@gmail.com>
2026-09-08 00:06:01 +0100

    Added more behaviours to teacher object, all tested

--- StudentGrade/src/student/Teacher.java
+++ StudentGrade/src/student/Teacher.java
@@ -5,12 +5,68 @@ import java.util.ArrayList;
 public class Teacher {
     private ArrayList<Student> gradeBook = new ArrayList<>();
 
+    public ArrayList<Student> checkGradeBook() {return gradeBook;}
 
-    public ArrayList<Student> checkGradeBook() {
-        return gradeBook;
+    public void addStudent(Student newStudent) {gradeBook.add(newStudent);}
+
+    public double getStudentTotalGrade(Student student) {
+        double sum = 0;
+        for(int studentGrade : student.getGrades()) sum += studentGrade;
+        return sum;
     }
 
-    public void addStudent(Student newStudent) {
-        gradeBook.add(newStudent);
+    public Student getStudent(int studentID) {return gradeBook.get(studentID - 1);}
+
+    public double getStudentAverage(Student student1) {return Math.floor((getStudentTotalGrade(student1)/student1.getGrades().length) * 100) / 100;}
+
+    public double [] getStudentsAverage() {
+        double [] studentAverage = new double[gradeBook.size()];
+        for(int eachStudent = 1; eachStudent <= gradeBook.size(); eachStudent++){studentAverage[eachStudent - 1] = getStudentAverage(getStudent(eachStudent));}
+        return  studentAverage;
+    }
+
+
+    public int [] getStudentPositions() {
+        double [] averageGrades = getStudentsAverage();
+        int [] studentPositions = new int[gradeBook.size()];
+        for(int eachStudent = 0; eachStudent < studentPositions.length; eachStudent++){studentPositions[eachStudent] = eachStudent;}
+
+        for(int index = 0; index < gradeBook.size(); index++){
+            for(int count = 1; count < gradeBook.size(); count++){
+                if(averageGrades[index] < averageGrades[count]){
+                    int store = studentPositions[index];
+                    studentPositions[index] = studentPositions[count];
+                    studentPositions[count] = store;
+                }
+            }
+        }
+
+        for(int index = 0; index < studentPositions.length; index++){++studentPositions[index];}
+
+        return studentPositions;
+    }
+
+    public int getStudentHighestScore(Student student1) {
+        int highestScore = 0;
+        for(int studentGrade : student1.getGrades()){if(studentGrade > highestScore){highestScore = studentGrade;}}
+        return highestScore;
+    }
+
+    public int getStudentLowestScore(Student student1) {
+        int lowestScore = student1.getGrades()[0];
+        for(int studentGrade : student1.getGrades()){if(studentGrade < lowestScore){lowestScore = studentGrade;}}
+        return lowestScore;
+    }
+
+    public int getStudentSubjectsPassed(Student student1) {
+        int subjectsPassed = 0;
+        for(int studentGrade : student1.getGrades()){if(studentGrade >= 50){subjectsPassed++;}}
+        return subjectsPassed;
+    }
+
+    public int getStudentSubjectsFailed(Student student1) {
+        int subjectsFailed = 0;
+        for(int studentGrade : student1.getGrades()){if(studentGrade < 50){subjectsFailed++;}}
+        return subjectsFailed;
     }
 }
--- StudentGrade/test/student/TeacherTest.java
+++ StudentGrade/test/student/TeacherTest.java
@@ -3,6 +3,7 @@ package student;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class TeacherTest {
@@ -35,5 +36,135 @@ public class TeacherTest {
     }
 
     @Test
+    public void testThatTeacherWhenOneStudentIsAdded_AndSubjectsAndGradesAreAdded_TeacherCanGetTotalGrade(){
+        Student student1 = new Student();
+        student1.registerNumberOfSubjects(3);
+        student1.addGrade(1,50);
+        student1.addGrade(2,75);
+        student1.addGrade(3,65);
+        teacher.addStudent(student1);
+        assertEquals(190.0, teacher.getStudentTotalGrade(student1));
+    }
 
+    @Test
+    public void testThatWhenOneStudentIsAdded_AndSubjectsAndGradesAreAdded_TeacherCanGetAverageGrade(){
+        Student student1 = new Student();
+        student1.registerNumberOfSubjects(3);
+        student1.addGrade(1,50);
+        student1.addGrade(2,75);
+        student1.addGrade(3,65);
+        teacher.addStudent(student1);
+        assertEquals(63.33,teacher.getStudentAverage(student1));
+    }
+
+    @Test
+    public void testThatWhenOneStudentIsAdded_AndTeacherGetStudent_StudentIsRegisteredInGradeBook(){
+        Student student1 = new Student();
+        teacher.addStudent(student1);
+        assertEquals(1,teacher.checkGradeBook().size());
+    }
+
+
+    @Test
+    public void testThatWhenIAddStudents_AndSubjectsAndGradesAreAdded_WhenICheckStudentsAverageIGetAverageGradeOfEachStudent(){
+        Student student1 = new Student();
+        student1.registerNumberOfSubjects(3);
+        student1.addGrade(1,50);
+        student1.addGrade(2,75);
+        student1.addGrade(3,65);
+        teacher.addStudent(student1);
+        Student  student2 = new Student();
+        student2.registerNumberOfSubjects(3);
+        student2.addGrade(1,60);
+        student2.addGrade(2,25);
+        student2.addGrade(3,55);
+        teacher.addStudent(student2);
+        double [] expectedAverageGrades = {63.33, 46.66};
+        assertArrayEquals(expectedAverageGrades, teacher.getStudentsAverage());
+    }
+
+    @Test
+    public void tesThatWhenIAddStudents_AndSubjectsAndGradesAreAdded_WhenIGetStudentsAverage_IGetPositionOfEachStudent(){
+        Student student1 = new Student();
+        student1.registerNumberOfSubjects(3);
+        student1.addGrade(1,50);
+        student1.addGrade(2,75);
+        student1.addGrade(3,65);
+        teacher.addStudent(student1);
+        Student  student2 = new Student();
+        student2.registerNumberOfSubjects(3);
+        student2.addGrade(1,60);
+        student2.addGrade(2,25);
+        student2.addGrade(3,55);
+        teacher.addStudent(student2);
+        double [] expectedAverageGrades = {63.33, 46.66};
+        assertArrayEquals(expectedAverageGrades, teacher.getStudentsAverage());
+        int [] expectedPositions = {1,2};
+        assertArrayEquals(expectedPositions, teacher.getStudentPositions());
+    }
+
+    @Test
+    public void tesThatWhenIAddStudents_AndSubjectsAndGradesAreAdded_WhenIGetStudentsAverage_IGetPositionOfEachStudentTest2(){
+        Student student1 = new Student();
+        student1.registerNumberOfSubjects(3);
+        student1.addGrade(1,60);
+        student1.addGrade(2,25);
+        student1.addGrade(3,55);
+        teacher.addStudent(student1);
+        Student  student2 = new Student();
+        student2.registerNumberOfSubjects(3);
+        student2.addGrade(1,50);
+        student2.addGrade(2,75);
+        student2.addGrade(3,65);
+        teacher.addStudent(student2);
+        double [] expectedAverageGrades = {46.66,63.33};
+        assertArrayEquals(expectedAverageGrades, teacher.getStudentsAverage());
+        int [] expectedPositions = {2,1};
+        assertArrayEquals(expectedPositions, teacher.getStudentPositions());
+    }
+
+    @Test
+    public void testThatWhenIAddOneStudent_AndSubjectsAndGrades_WhenICheckForHighestGrade_IGetHighestGrade(){
+        Student student1 = new Student();
+        student1.registerNumberOfSubjects(3);
+        student1.addGrade(1,50);
+        student1.addGrade(2,75);
+        student1.addGrade(3,65);
+        teacher.addStudent(student1);
+        assertEquals(75,teacher.getStudentHighestScore(student1));
+    }
+
+    @Test
+    public void testThatWhenIAddOneStudent_AndSubjectsAndGrades_WhenICheckForLowestGrade_IGetLowestGrade(){
+        Student student1 = new Student();
+        student1.registerNumberOfSubjects(3);
+        student1.addGrade(1,50);
+        student1.addGrade(2,75);
+        student1.addGrade(3,65);
+        teacher.addStudent(student1);
+        assertEquals(50,teacher.getStudentLowestScore(student1));
+    }
+
+    @Test
+    public void testThatWhenIAddOneStudent_AndSubjectsAndGrades_WhenICheckSubjectsPassed_IGetNumberOfSubjectsPassed(){
+        Student student1 = new Student();
+        student1.registerNumberOfSubjects(3);
+        student1.addGrade(1,50);
+        student1.addGrade(2,75);
+        student1.addGrade(3,65);
+        teacher.addStudent(student1);
+        assertEquals(3,teacher.getStudentSubjectsPassed(student1));
+    }
+
+    @Test
+    public void testThatWhenIAddOneStudent_AndSubjectsAndGrades_WhenICheckStudentsSubjectsFailed_IGetNumberOfSubjectsFailed(){
+        Student student1 = new Student();
+        student1.registerNumberOfSubjects(3);
+        student1.addGrade(1,20);
+        student1.addGrade(2,40);
+        student1.addGrade(3,60);
+        teacher.addStudent(student1);
+        assertEquals(2, teacher.getStudentSubjectsFailed(student1));
+
+    }
 }
