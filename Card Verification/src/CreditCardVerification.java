c8309caefd7755445bc0100bc398720c7b140f81
panthera2020 <diranoye2020@gmail.com>
2026-09-14 20:14:41 +0100

    Tested for Valid length, Valid card digit, card type and card validity

--- Card Verification/src/CreditCardVerification.java
+++ Card Verification/src/CreditCardVerification.java
@@ -3,9 +3,7 @@ public class CreditCardVerification {
     public boolean isLengthValid(long cardNumber) {
         boolean isCreditCardValid = false;
         int counter = getCardLength(cardNumber);
-        if(counter >= 13 && counter <= 16){
-            isCreditCardValid = true;
-        }
+        if(counter >= 13 && counter <= 16){ isCreditCardValid = true; }
         return isCreditCardValid;
     }
 
@@ -22,7 +20,6 @@ public class CreditCardVerification {
         String cardNumberString = cardNumber + "";
         char firstDigit = cardNumberString.charAt(0);
         char secondDigit = cardNumberString.charAt(1);
-
         return firstDigit == '4' || firstDigit == '5' || firstDigit == '6' || (firstDigit == '3' && secondDigit == '7');
     }
 
@@ -32,15 +29,10 @@ public class CreditCardVerification {
         char secondDigit = cardNumberString.charAt(1);
 
         String cardType = "";
-        if(firstDigit == '4'){
-            cardType = "Visa";
-        }if(firstDigit == '5'){
-            cardType = "MasterCard";
-        }if(firstDigit == '6'){
-            cardType = "Discover Card";
-        }if(firstDigit == '3' && secondDigit == '7'){
-            cardType = "American Express Card";
-        }
+        if(firstDigit == '4'){ cardType = "Visa";}
+        if(firstDigit == '5'){ cardType = "MasterCard";}
+        if(firstDigit == '6'){ cardType = "Discover Card"; }
+        if(firstDigit == '3' && secondDigit == '7'){ cardType = "American Express Card"; }
         return cardType;
     }
 
@@ -48,17 +40,13 @@ public class CreditCardVerification {
         String numberToString = numbers + "";
         String [] stringNumbersToArray = numberToString.split("");
         int [] arrayOfNumbers = new int[stringNumbersToArray.length];
-        for(int index = 0; index < stringNumbersToArray.length; index++){
-            arrayOfNumbers[index] = Integer.parseInt(stringNumbersToArray[index]);
-        }
+        for(int index = 0; index < stringNumbersToArray.length; index++){ arrayOfNumbers[index] = Integer.parseInt(stringNumbersToArray[index]); }
         return arrayOfNumbers;
     }
 
     public int [] doubleSecondElementsRightToLeft(long numbers) {
         int [] arrayOfNumbers = getArrayOf(numbers);
-        for(int index = arrayOfNumbers.length - 2; index >= 0; index-=2){
-            arrayOfNumbers[index] = arrayOfNumbers[index] * 2;
-        }
+        for(int index = arrayOfNumbers.length - 2; index >= 0; index-=2){ arrayOfNumbers[index] = arrayOfNumbers[index] * 2;}
         return arrayOfNumbers;
     }
 
@@ -82,18 +70,14 @@ public class CreditCardVerification {
     public int sumOfSecondDigitsRightToLeft(long numbers) {
         int []  arrayOfNumbers = doubleSecondElementsRightToLeftNoDoubleDigitElement(numbers);
         int sum = 0;
-        for(int index = arrayOfNumbers.length - 2; index >= 0; index-=2){
-            sum += arrayOfNumbers[index];
-        }
+        for(int index = arrayOfNumbers.length - 2; index >= 0; index-=2){ sum += arrayOfNumbers[index];}
         return sum;
     }
 
     public int sumOfOddPlacedDigitsRightToLeft(long numbers) {
         int []  arrayOfNumbers = doubleSecondElementsRightToLeftNoDoubleDigitElement(numbers);
         int sum = 0;
-        for(int index = arrayOfNumbers.length - 1; index >= 0; index-=2){
-            sum += arrayOfNumbers[index];
-        }
+        for(int index = arrayOfNumbers.length - 1; index >= 0; index-=2){sum += arrayOfNumbers[index];}
         return sum;
     }
 
