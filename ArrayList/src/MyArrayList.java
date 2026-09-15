ededa48ac7c58ceeb9607909cc4f6b0ceeca6f7c
panthera2020 <diranoye2020@gmail.com>
2026-09-15 23:33:43 +0100

    added size and remove function

--- ArrayList/src/MyArrayList.java
+++ ArrayList/src/MyArrayList.java
@@ -2,23 +2,27 @@ public class MyArrayList {
     private Object[] elements = new Object[10];
     private int size = 0;
 
-    public boolean isEmpty() {
-        return size == 0;
-    }
+    public boolean isEmpty() { return size == 0; }
 
     public void add(Object element){
         if(size == elements.length - 1){
-            Object[] temp = new Object[elements.length*2];
-            this.elements = temp;
+            this.elements = new Object[elements.length*2];
             elements[size++] = element;
-        }else{
-            elements[size++] = element;
-        }
+        }else{ elements[size++] = element; }
 
     }
-    public Object get(int index){
-        return elements[index];
+    public Object get(int index){ return elements[index];}
+
+    public void add(int index, Object element) {
+        if(index > size - 1){ add(element);}
+        else{ elements[index] = element; }
     }
 
+    public int size(){ return size; }
 
+    public void remove(int index) { elements[index] = null;}
+
+    public void removeAll() {
+        for(int index = 0; index < size; index++){ elements[index] = null; }
+    }
 }
--- ArrayList/test/ArrayListTest.java
+++ ArrayList/test/ArrayListTest.java
@@ -1,8 +1,7 @@
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class ArrayListTest {
     private MyArrayList arrayList;
@@ -41,5 +40,56 @@ public class ArrayListTest {
         assertEquals(10,arrayList.get(100));
     }
 
+    @Test
+    public void testThatICanAddElementsAtSpecificIndex(){
+        Object element = 10;
+        for(int index = 0; index < 12; index++){
+            arrayList.add(element);
+        }
+        arrayList.add(10, 12);
+        assertEquals(12,arrayList.get(10));
+    }
 
+    @Test
+    public void testThatWhenIAddElementToIndexBelowZero_ErrorIsThrown(){
+        arrayList.add(10);
+        arrayList.add(12);
+        assertThrows(IndexOutOfBoundsException.class, ()-> arrayList.add(-1, 10));
+    }
+
+    @Test
+    public void testThatWhenIAddElementToIndexAboveSize_ElementIsAddedToEndOfArrayList(){
+        arrayList.add(10);
+        arrayList.add(12);
+        arrayList.add(6,13);
+        assertEquals(13,arrayList.get(2));
+    }
+    @Test
+    public void testThatICanGetTheSizeOfElementsAddedToArrayList(){
+        Object element = 10;
+        for(int index = 0; index < 12; index++){
+            arrayList.add(element);
+        }
+        assertEquals(12,arrayList.size());
+    }
+
+    @Test
+    public void testThatWhenIRemoveELementFromASpecificIndex_ElementIsRemoved(){
+        arrayList.add(10);
+        arrayList.add(12);
+        arrayList.add(6);
+        arrayList.remove(1);
+        assertNull(arrayList.get(1));
+    }
+
+    @Test
+    public void testThatWhenIRemoveAll_AllElementsAreRemoved(){
+        arrayList.add(10);
+        arrayList.add(12);
+        arrayList.add(6);
+        arrayList.removeAll();
+        for(int index = 0; index < arrayList.size(); index++){
+            assertNull(arrayList.get(index));
+        }
+    }
 }
