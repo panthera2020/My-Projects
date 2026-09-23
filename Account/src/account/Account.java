b4b8f2454bc4f9d9b799c47ca1c9d1695e727ff0
panthera2020 <diranoye2020@gmail.com>
2026-09-23 21:40:22 +0100

    Password features added and tested

--- Account/src/account/Account.java
+++ Account/src/account/Account.java
@@ -2,10 +2,18 @@ package account;
 
 public class Account {
     private double balance = 0.0;
+    private String password;
 
-    public double checkBalance() {return balance;}
+    public Account(String password) {this.password = password;}
 
-    public void deposit(double amount) {if(amount > 0) balance += amount;}
+    public double checkBalance(String password) {
+        if (password.equals(this.password)) { return balance;}
+        else{throw new IllegalArgumentException("Wrong password");}
+    }
 
-    public void withdraw(double amount) {if(balance >= amount) balance -= amount;}
+    public void deposit(double amount, String password) {if(amount > 0 && password.equals(this.password)) balance += amount;}
+
+    public void withdraw(double amount, String password) {if(balance >= amount && password.equals(this.password)) balance -= amount;}
+
+    public void changePassword(String newPassword) {this.password = newPassword;}
 }
--- Account/test/account/AccountTest.java
+++ Account/test/account/AccountTest.java
@@ -4,64 +4,91 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class AccountTest {
     private Account account;
 
     @BeforeEach
     public void createAccount() {
-        account = new Account();
+        account = new Account("1234");
+    }
+
+    @Test
+    public void testThatIcannotCheckBalanceWithoutCorrectPassword() {
+        assertThrows(IllegalArgumentException.class, ()-> account.checkBalance("1245"));
     }
 
     @Test
     public void testThatWhenICheckBalance_OfNewAccount_BalanceIsZero() {
-        assertEquals(0,account.checkBalance());
+        assertEquals(0,account.checkBalance("1234"));
+    }
+    @Test
+    public void testThatICannotDepositWithoutCorrectPassword() {
+        assertEquals(0,account.checkBalance("1234"));
+        account.deposit(5000,"1245");
+        assertEquals(0,account.checkBalance("1234"));
     }
 
     @Test
     public void testThatWhenIDeposit5k_BalanceIs5k() {
-        assertEquals(0,account.checkBalance());
-        account.deposit(5000.0);
-        assertEquals(5000,account.checkBalance());
+        assertEquals(0,account.checkBalance("1234"));
+        account.deposit(5000.0,"1234");
+        assertEquals(5000,account.checkBalance("1234"));
     }
     @Test
     public void testThatWhenIDeposit5kTwice_BalanceIs10k() {
-        assertEquals(0,account.checkBalance());
-        account.deposit(5000);
-        assertEquals(5000,account.checkBalance());
-        account.deposit(5000);
-        assertEquals(10000,account.checkBalance());
+        assertEquals(0,account.checkBalance("1234"));
+        account.deposit(5000, "1234");
+        assertEquals(5000,account.checkBalance("1234"));
+        account.deposit(5000,"1234");
+        assertEquals(10000,account.checkBalance("1234"));
     }
 
     @Test
     public void testThatWhenIDepositNegative1k_BalanceIsZero() {
-        assertEquals(0,account.checkBalance());
-        account.deposit(-5000);
-        assertEquals(0,account.checkBalance());
+        assertEquals(0,account.checkBalance("1234"));
+        account.deposit(-5000,"1234");
+        assertEquals(0,account.checkBalance("1234"));
     }
+
+    @Test
+    public void testThatWhenIDepositICannotWithrawWithoutCorrectPassword() {
+        account.deposit(5000,"1234");
+        account.withdraw(2000, "1255");
+        assertEquals(5000,account.checkBalance("1234"));
+    }
+
     @Test
     public void testThatWhenITryToWithdrawFromNewAccount_BalanceIsZero() {
-        assertEquals(0,account.checkBalance());
-        account.withdraw(5000.0);
-        assertEquals(0,account.checkBalance());
+        assertEquals(0,account.checkBalance("1234"));
+        account.withdraw(5000.0,"1234");
+        assertEquals(0,account.checkBalance("1234"));
     }
 
     @Test
     public void testThatWhenIDeposit5k_AndWithdraw2k_BalanceIs3k() {
-        assertEquals(0,account.checkBalance());
-        account.deposit(5000);
-        assertEquals(5000,account.checkBalance());
-        account.withdraw(2000);
-        assertEquals(3000,account.checkBalance());
+        account.deposit(5000,"1234");
+        assertEquals(5000,account.checkBalance("1234"));
+        account.withdraw(2000,"1234");
+        assertEquals(3000,account.checkBalance("1234"));
     }
 
     @Test
     public void testThatWhenIDeposit5k_AndWithdraw7k_BalanceIs5k() {
-        assertEquals(0,account.checkBalance());
-        account.deposit(5000);
-        assertEquals(5000,account.checkBalance());
-        account.withdraw(7000);
-        assertEquals(5000,account.checkBalance());
+        assertEquals(0,account.checkBalance("1234"));
+        account.deposit(5000.0,"1234");
+        assertEquals(5000,account.checkBalance("1234"));
+        account.withdraw(7000.0,"1234");
+        assertEquals(5000,account.checkBalance("1234"));
     }
+
+    @Test
+    public void testThatWhenIChangePassword_PasswordIsChanged() {
+        assertEquals(0,account.checkBalance("1234"));
+        account.changePassword("5555");
+        assertEquals(0,account.checkBalance("5555"));
+    }
+
 
 }
